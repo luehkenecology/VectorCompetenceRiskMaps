9f039d1c1bfe919e131349829d0c2e081cc17983
luehkenecology <luehken65@gmail.com>
2017-12-10 21:57:41 +0100

    solved problem regarding NA values --> reading raw data, converting NA to a value out of range (=240), then solve by reclassifying

--- generate-maps.R
+++ generate-maps.R
@@ -74,28 +74,37 @@ plot(alb2, border = "gray", col = "red", add = T)
 plot(mne2, border = "gray", col = "red", add = T)
 plot(bih, border = "gray", col = "red", add = T)
 plot(country_shapes_nuts0_all_crop, col = NA, add = T)
-points(cbind(7.843,	48.016), lwd = 2, col = "green", cex = 2.8)
-points(cbind(8.652,	49.411), lwd = 2, col = "orange", cex = 2.8)
-points(cbind(16.837,	39.473), lwd = 2, col = "blue", cex = 2.8)
+points(cbind(7.843,	48.016), lwd = 2, col = "green", cex = 2.6)
+points(cbind(8.652,	49.411), lwd = 2, col = "orange", cex = 2.6)
+points(cbind(16.837,	39.473), lwd = 2, col = "blue", cex = 2.6)
 dev.off()
 
 # temperature data----------------------------------------
 temp_14d_21C <- raster("output/zika_mean_map_14_21.grd")
 country_shapes_nuts0_all_crop <- crop(country_shapes_nuts0_all, cropping_info)
-temp_14d_21C_crop <- crop(temp_14d_21C, cropping_info)
+
+testst <- lapply(2007:2016, function(x) stack(paste("C:/Users/RenkeLuehken/Google Drive/Research_projects/Extract_E_OBS_gridded_dataset/output/tg_0.25deg_reg_v14.0_europe_", 
+                                                    x, ".grd", sep = "")))
+testst2 <- stack(testst)
+testst3 <- crop(testst2, cropping_info)
+testst3[[1]][is.na((testst3[[1]]))] <- 240
+
+#temp_14d_21C_crop <- crop(temp_14d_21C, cropping_info)
+
+temp_14d_21C_creece <- crop(temp_14d_21C, country_shapes_nuts0_all)
+temp_14d_21C[is.na((temp_14d_21C))] <- 240
+temp_14d_21C_creece2 <- mask(temp_14d_21C_creece,country_shapes_nuts0_all)
 
 clip<-function(raster,shape) {
   cr <- crop(raster, extent(shape), snap="out")                    
   fr <- rasterize(shape, cr, getCover = T)   
   lr <- mask(x=cr, mask=fr)
-  
 }
 
-clip<-function(raster,shape) {
- 
-}
 
 temp_14d_21C_mask_all <- mask(temp_14d_21C, country_shapes_nuts0_all_crop)
+eb <- stack(temp_14d_21C_mask_all, testst3[[1]])
+temp_14d_21C_mask_all <- max(eb)
 temp_14d_21C_mask_positive <- mask(temp_14d_21C, positive_nuts_crop)
 
 # NAs to zero
@@ -109,7 +118,8 @@ m <- c(-Inf,0,0,
        30, 60, 2,
        60, 90, 3,
        90, 120, 4,
-       120, 160, 5)
+       120, 160, 5,
+       160, 250, 6)
 
 rclmat <- matrix(m, ncol=3, byrow=TRUE)
 
@@ -118,14 +128,18 @@ temp_14d_21C_mask_all3 <- reclassify(temp_14d_21C_mask_all2,
 temp_14d_21C_mask_positive3 <- reclassify(temp_14d_21C_mask_positive2, 
                                      rclmat)
 
+
+
 rbPal <- colorRampPalette(c("yellow","red","purple"))
+
+
 
 png(file = "figs/transmission_period.png",
     width = 7, height = 4.8, units = 'in', res = 1000)
 par(mar=c(2.5,2.5,1.5,1.5))
 plot(country_shapes_nuts0_all_crop, cex.axis=0.9, axes = T)
 plot(temp_14d_21C_mask_all3, legend = F, add = T, col = c("white",
-                                      rbPal(5)))
+                                      rbPal(5), "gray"))
 plot(country_shapes_nuts0_all_crop, add = T, col = NA)
 legend("bottom", legend = c("0 d",
                             ">0-30 d",
@@ -134,7 +148,7 @@ legend("bottom", legend = c("0 d",
                             ">90-120 d",
                             ">120-160 d"),horiz = T, 
        cex = 0.805, bg = "white",
-       fill = c("white", rbPal(6)))
+       fill = c("white", rbPal(5)))
 points(cbind(7.843,	48.016), lwd = 2, 
        col = "green", bg = rbPal(5)[extract(temp_14d_21C_mask_all3, cbind(7.843,	48.016))], cex = 2.6, pch = 21)
 points(cbind(8.652,	49.411), lwd = 2, 
@@ -150,14 +164,12 @@ clip <- function(shape_dat, raster_dat){
   raster_dat_2[fr==0] <- 0 
   raster_dat_3 <- reclassify(raster_dat_2,
              cbind(NA, 0))
-  reclassify(raster_dat_3, 
-                                           rclmat, right=NA)
 }
 
-temp_14d_21C_mask_reclass2 <- clip(positive_nuts_crop, temp_14d_21C_crop)
-alb5 <- clip(alb2, temp_14d_21C_crop)
-mne5 <- clip(mne2, temp_14d_21C_crop)
-bih5 <- clip(bih, temp_14d_21C_crop)
+temp_14d_21C_mask_reclass2 <- clip(positive_nuts_crop, temp_14d_21C_mask_all3)
+alb5 <- clip(alb2, temp_14d_21C_mask_all3)
+mne5 <- clip(mne2, temp_14d_21C_mask_all3)
+bih5 <- clip(bih, temp_14d_21C_mask_all3)
 
 eb <- stack(temp_14d_21C_mask_reclass2, alb5, mne5, bih5)
 er <- max(eb)
@@ -168,7 +180,7 @@ png(file = "figs/merge_albopictus_distribution-transmission_period.png",
 par(mar=c(2.5,2.5,1.5,1.5))
 plot(country_shapes_nuts0_all_crop, cex.axis=0.9, axes = T)
 plot(er, add = T, 
-     col = c("white", rbPal(5)), legend = F)
+     col = c("white", rbPal(5), "gray"), legend = F)
 #plot(alb5, legend = F, add = T, col = c("white",
 #                                        rbPal(5)[1:3]))
 #plot(mne5, legend = F, add = T, col = c("white",
